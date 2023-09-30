f5a6372cc6bc4ede4f742e3bb9d9bcf6dc4681ce
aliyaylali <aliyaylalibjk@gmail.com>
2023-09-30 18:21:30 +0300

    first commit

---
+++ password-generate.py
@@ -0,0 +1,39 @@
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters= int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+letters_list = []
+number_list = []
+symbol_list = []
+password_list = []
+
+
+generator = []
+
+for letter in letters:
+  letters_list.append(letter)
+  random.shuffle(letters_list)
+  hand_letter = letters_list[0:nr_letters]
+generator.extend(hand_letter)
+
+for symbol in symbols:
+  symbol_list.append(symbol)
+  random.shuffle(symbol_list)
+  hand_symbol = symbol_list[0:nr_symbols]
+generator.extend(hand_symbol)
+
+for number in numbers:
+  number_list.append(number)
+  random.shuffle(number_list)
+  hand_number = number_list[0:nr_numbers]
+generator.extend(hand_number)
+
+random.shuffle(generator)
+
+print(f"Here is your password: 3{''.join(generator)}")
